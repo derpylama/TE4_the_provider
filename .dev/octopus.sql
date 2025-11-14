0c08954895446d5a698b9c538b8ec3b4d9032f48
SimonAndersson0 <151626143+SimonAndersson0@users.noreply.github.com>
2025-11-14 10:19:54 +0100

    test

--- .dev/octopus.sql
+++ .dev/octopus.sql
@@ -1,21 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Värd: 127.0.0.1
--- Tid vid skapande: 14 nov 2025 kl 09:40
--- Serverversion: 10.4.32-MariaDB
--- PHP-version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET FOREIGN_KEY_CHECKS = 0;
+-- Current
+DROP TABLE IF EXISTS ban, blog, event, event_invite, img, organisation, rule, user, wiki, wiki_changes;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
+
 
 --
 -- Databas: `octopus`
@@ -28,28 +17,28 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `ban` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `user_id` int(11) NOT NULL,
-  `start_time` datetime NOT NULL,
+  `start_time` datetime DEFAULT current_timestamp(),
   `end_time` datetime NOT NULL,
   `blog` tinyint(1) NOT NULL DEFAULT 0,
   `wiki` tinyint(1) NOT NULL DEFAULT 0,
   `calendar` tinyint(1) NOT NULL DEFAULT 0,
   `reason` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
 --
 -- Tabellstruktur `blog`
---
+-- 
 
 CREATE TABLE `blog` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `content` text DEFAULT NULL,
   `title` varchar(100) NOT NULL,
   `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -58,13 +47,13 @@ CREATE TABLE `blog` (
 --
 
 CREATE TABLE `event` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `user_id` int(11) NOT NULL,
-  `start_time` datetime NOT NULL,
+  `start_time` datetime DEFAULT current_timestamp(),
   `event_info` text DEFAULT NULL,
   `title` int(11) NOT NULL,
   `end_time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -73,11 +62,11 @@ CREATE TABLE `event` (
 --
 
 CREATE TABLE `event_invite` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `event_id` int(11) NOT NULL,
   `invited_user_id` int(11) NOT NULL,
   `accepted` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -86,11 +75,11 @@ CREATE TABLE `event_invite` (
 --
 
 CREATE TABLE `img` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `img_url` varchar(2000) NOT NULL,
   `blog_id` int(11) DEFAULT NULL,
   `wiki_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -99,7 +88,7 @@ CREATE TABLE `img` (
 --
 
 CREATE TABLE `organisation` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(100) NOT NULL,
   `mail` varchar(100) NOT NULL,
   `phone_number` varchar(100) NOT NULL,
@@ -107,7 +96,7 @@ CREATE TABLE `organisation` (
   `username` varchar(100) NOT NULL,
   `adress` varchar(100) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -116,15 +105,15 @@ CREATE TABLE `organisation` (
 --
 
 CREATE TABLE `rule` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `rule` varchar(200) NOT NULL,
   `description` text DEFAULT NULL,
   `blog` tinyint(1) NOT NULL DEFAULT 0,
   `wiki` tinyint(1) NOT NULL DEFAULT 0,
   `calendar` tinyint(1) NOT NULL DEFAULT 0,
-  `creation_date` datetime NOT NULL,
-  `latest_update` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `creation_date` datetime DEFAULT current_timestamp(),
+  `latest_update` datetime DEFAULT current_timestamp()
+);
 
 -- --------------------------------------------------------
 
@@ -133,7 +122,7 @@ CREATE TABLE `rule` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `org_id` int(11) NOT NULL,
   `mail` varchar(100) DEFAULT NULL,
   `adress` varchar(100) DEFAULT NULL,
@@ -142,7 +131,7 @@ CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `type` enum('admin','end_user','user') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -151,11 +140,11 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `wiki` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `org_id` int(11) NOT NULL,
   `content` mediumtext DEFAULT NULL,
   `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -164,142 +153,11 @@ CREATE TABLE `wiki` (
 --
 
 CREATE TABLE `wiki_changes` (
-  `id` int(11) NOT NULL,
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `time` datetime NOT NULL,
   `new_content` text NOT NULL,
   `user_id` int(11) NOT NULL,
   `old_content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
---
--- Index för dumpade tabeller
---
 
---
--- Index för tabell `ban`
---
-ALTER TABLE `ban`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `blog`
---
-ALTER TABLE `blog`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `event`
---
-ALTER TABLE `event`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `event_invite`
---
-ALTER TABLE `event_invite`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `img`
---
-ALTER TABLE `img`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `organisation`
---
-ALTER TABLE `organisation`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `rule`
---
-ALTER TABLE `rule`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `wiki`
---
-ALTER TABLE `wiki`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index för tabell `wiki_changes`
---
-ALTER TABLE `wiki_changes`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT för dumpade tabeller
---
-
---
--- AUTO_INCREMENT för tabell `ban`
---
-ALTER TABLE `ban`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `blog`
---
-ALTER TABLE `blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `event`
---
-ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `event_invite`
---
-ALTER TABLE `event_invite`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `img`
---
-ALTER TABLE `img`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `organisation`
---
-ALTER TABLE `organisation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `rule`
---
-ALTER TABLE `rule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `wiki`
---
-ALTER TABLE `wiki`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT för tabell `wiki_changes`
---
-ALTER TABLE `wiki_changes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
