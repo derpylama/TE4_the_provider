333a58e2d3c37baf5c3fd43fea7d62797a9430d2
SimonAndersson0 <151626143+SimonAndersson0@users.noreply.github.com>
2025-11-17 13:29:47 +0100

    created a default template for endpoints

--- api/wiki-api/wiki-api-handler.php
+++ api/wiki-api/wiki-api-handler.php
@@ -1,11 +1,31 @@
 <?php
 
 require_once('../api-handler.php');
-class UserApiHandler extends BaseApiHandler{
+class WikiApiHandler extends BaseApiHandler{
 
     public function getUsers() {//example method
         $stmt = $this->conn->query("SELECT * FROM users");
         return $stmt->fetchAll();
+    }
+    public function createWiki($title, $content, $author_id) {
+        return json_encode([
+            "status" => "success", 
+            "message" => "Wiki currently disabled.",
+            "title" => $title,
+            "content" => $content,
+            "author_id" => $author_id
+        ]); 
+/* 
+        $stmt = $this->conn->prepare("INSERT INTO wikis (title, content, author_id) VALUES (:title, :content, :author_id)");
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':author_id', $author_id);
+        if ($stmt->execute()) {
+            return ['status' => 'success', 'wiki_id' => $this->conn->lastInsertId()];
+        } else {
+            return ['status' => 'error', 'message' => 'Failed to create wiki'];
+        }
+         */
     }
 }
 
