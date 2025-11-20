18fa990e905a3caf857b562403020a458dc0a944
derpylama <98833155+derpylama@users.noreply.github.com>
2025-11-20 08:41:13 +0100

    now able to get either one blog or all blogs in a org

--- api/blog-api/get-blog.php
+++ api/blog-api/get-blog.php
@@ -1,1 +1,29 @@
 <?php 
+
+require_once __DIR__ . "/blog-api-handler.php";
+require_once __DIR__ . "/../auth-api/auth-api-handler.php";
+
+header('Content-Type: application/json');
+
+$authHandler = new AuthApiHandler();
+$blogHandler = new BlogApiHandler();
+
+
+$blogData = json_decode(file_get_contents("php://input"), true);
+
+$verifyResult = json_decode($authHandler->verifyAuthToken($blogData["token"]), true);
+
+if ($verifyResult["status"] == "success") {
+    if ((isset($verifyResult) && !empty($verifyResult)) && (isset($blogData["blogId"]) && !empty($blogData["blogId"]))) {
+        echo $blogHandler->getBlog($verifyResult[0]["customer_id"], $blogData["blogId"]);
+    }
+    else if (isset($verifyResult) && !empty($verifyResult)) {
+        echo $blogHandler->getBlog($verifyResult[0]["customer_id"]);
+    }
+    
+}
+else {
+    echo $authHandler->verifyAuthToken($blogData["token"]);
+}
+
+
