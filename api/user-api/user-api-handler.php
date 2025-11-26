5a6ce69e949b2d449c6f2e08ba2f5f43cbb58d60
derpylama <98833155+derpylama@users.noreply.github.com>
2025-11-26 11:40:13 +0100

    fixed edit for general info in user and calendar

--- api/user-api/user-api-handler.php
+++ api/user-api/user-api-handler.php
@@ -209,7 +209,7 @@ class UserApiHandler extends BaseApiHandler{
             ]);
         }  
     }
-    public function editUser($token, $editUserId, $mail, $adress, $employmentNumber, $birthDate, $username, $password, $type) {
+    public function editUser($token, $editUserId, $mail, $adress, $employmentNumber, $birthDate, $username, $password, $type, $general) {
         //Token---------------------------------------------------------------
         $tokeninfo=$this->checkServiceAndToken($token); 
         if($tokeninfo['status']!="success"){
@@ -228,7 +228,12 @@ class UserApiHandler extends BaseApiHandler{
         $customerId=$tokeninfo["customer_id"];
         $id=$editUserId ?? $tokeninfo["userId"];
         
-
+        if ($editUserId == null) {
+            return json_encode([
+                "status" => "error",
+                "message" => "No user id to edit specified"
+            ]);
+        }
         
         try {
             if ($password != null) {
@@ -256,7 +261,8 @@ class UserApiHandler extends BaseApiHandler{
                 "birthdate" => $birthDate,
                 "username" => $username,
                 "password" => $newPassword,
-                "type" => $type
+                "type" => $type,
+                "general" => $general
             ];
 
             $editStringList = [];
@@ -283,7 +289,7 @@ class UserApiHandler extends BaseApiHandler{
         } catch (PDOException $e) {
             return json_encode([
                 "status" => "error",
-                "message" => "GRUB Database error: " . $e->getMessage()
+                "message" => "Database error: " . $e->getMessage()
             ]);
         }  
     }
