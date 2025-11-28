d1907d54ec77017ceeeb2c866406f61e1675532f
derpylama <98833155+derpylama@users.noreply.github.com>
2025-11-28 09:07:33 +0100

    fixed comments in sql file

--- .dev/octopus.sql
+++ .dev/octopus.sql
@@ -34,7 +34,7 @@ CREATE TABLE `user` (
   `latest_update` datetime DEFAULT current_timestamp()
 );
 
------------------------------------------------------------
+-- --------------------------------------------------------- 
 
 --
 -- Tabellstruktur `mail`
@@ -46,11 +46,11 @@ CREATE TABLE `mail` (
   `mail` varchar(100) NOT NULL,
   `creation_date` datetime DEFAULT current_timestamp(),
 
-  FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
-)
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
+);
 
 
-----------------------------------------------------------
+-- -------------------------------------------------------- 
 
 --
 -- Tabellstruktur `adress`
