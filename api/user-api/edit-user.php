1396b4327e79833346c4a58d9b2f20aecd9d5e71
SimonAndersson0 <151626143+SimonAndersson0@users.noreply.github.com>
2025-11-28 11:49:43 +0100

    apitest might be working

--- api/user-api/edit-user.php
+++ api/user-api/edit-user.php
@@ -6,17 +6,28 @@ header('Content-Type: application/json');
 
 $auth = new AuthApiHandler();
 $apiHandler = new UserApiHandler();
+
+// Get headers
+$header = getallheaders();
+
+// Check Authorization Header
+if (!isset($header["Authorization"])) {
+    $apiHandler->error("Missing Authorization Header", [], 401);
+    exit;
+}
+
+// Check if it is a Bearer Token
+if (substr($header["Authorization"], 0, 7) !== "Bearer ") {
+    $apiHandler->error("Invalid Authorization Header", [], 401);
+    exit;
+}
+
+$token = substr($header["Authorization"], 7);
+
 //get input data
 $input=json_decode(file_get_contents('php://input'), true);
 
-$reqparameter=['token'];
-foreach($reqparameter as $param){
-    if(!isset($input[$param])){
-        $message="Missing parameter: ".$param;
-        $apiHandler->error($message, [], 400);
-        exit;
-    }
-}
+
 
 $editUserId = $input["user_id"] ?? null;
 
