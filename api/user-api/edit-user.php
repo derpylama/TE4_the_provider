d70da208bea9746cecb8a97057e8cda853884e77
SimonAndersson0 <151626143+SimonAndersson0@users.noreply.github.com>
2025-12-02 11:20:59 +0100

    fixed edituser having wrong method

--- api/user-api/edit-user.php
+++ api/user-api/edit-user.php
@@ -28,7 +28,7 @@ $token = substr($header["Authorization"], 7);
 // $input=json_decode(file_get_contents('php://input'), true);
 
 // check if the request method is GET
-if ($_SERVER["REQUEST_METHOD"] !== "GET") {
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     $apiHandler->error("Invalid request method", [], 405);
     exit;
 }
